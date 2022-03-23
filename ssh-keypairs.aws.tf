62dda8c4f3ee5077286dd455d0c6c17d3b280d6c
Jason Witkowski <jwitko1@gmail.com>
2022-03-23 09:51:17 -0400

    Add comments to clarify resource conditionals, reformat aws-testing.tfvars

--- ssh-keypairs.aws.tf
+++ ssh-keypairs.aws.tf
@@ -1,15 +1,17 @@
 locals {
-  create_new_ssh_key = var.create_ssh_keypair && (length(var.ssh_public_key) > 0)
+  create_new_ssh_key  = var.create_ssh_keypair && (length(var.ssh_public_key) == 0)
+  create_aws_key_pair = (var.create_ssh_keypair && (length(var.ssh_public_key) > 0)) || local.create_new_ssh_key
 }
 
-# Generate SSH keypair
+# Generate RSA Key if create_ssh_keypair=true and No public key specified
 resource "tls_private_key" "generated_ssh_key_pair" {
   count     = local.create_new_ssh_key ? 1 : 0
   algorithm = "RSA"
 }
 
+# Create AWS Key Pair if create_ssh_keypair=true and No public key specified or create_ssh_keypair=true and key specified.
 resource "aws_key_pair" "ssh" {
-  count = var.create_ssh_keypair ? 1 : 0
+  count = local.create_aws_key_pair ? 1 : 0
 
   key_name_prefix = var.name
   public_key      = try(tls_private_key.generated_ssh_key_pair[0].public_key_openssh, var.ssh_public_key)
--- tests/aws-testing.tfvars
+++ tests/aws-testing.tfvars
@@ -1,2 +1,3 @@
-aws_tests                          = true
+aws_tests = true
+
 bootstrap_objectstorage_bucket_name = "test-cs-tf-userdata-launcher-bootstrap"
