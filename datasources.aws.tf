146a76315a19d6229a31484ff60b7387e1aaac17
Jason Witkowski <jwitko1@gmail.com>
2022-04-10 18:37:14 -0400

    Adding IAM roles and policies, ASG creation, and required datasources and variables.

--- datasources.aws.tf
+++ datasources.aws.tf
@@ -1,9 +1,16 @@
 locals {
   create_s3_bootstrap_policy = !var.code_package_public && var.cloud_provider == "aws" && length(var.bootstrap_objectstorage_bucket_name) != 0
+  use_aws_ubuntu_ami = var.operating_system == "ubuntu" && var.cloud_provider == "aws"
+  use_aws_external_ip = var.cloud_provider == "aws" && var.instance_external_ip
+  use_aws_ebs_volumes = var.cloud_provider == "aws" && length(var.ebs_block_device) > 0
+  ami_map = {
+    ubuntu = {
+      filters = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*"]
+      owners  = ["099720109477"]
+    }
+  }
 }
 
-# TODO: Remove this is the next PR
-# tflint-ignore: terraform_unused_declarations
 data "aws_iam_policy_document" "assume_role" {
   count = var.cloud_provider == "aws" ? 1 : 0
 
@@ -17,9 +24,22 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+# Policy allowing updating of Route53 records in internal zone
+data "aws_iam_policy_document" "route53_update" {
+  count = var.cloud_provider == "aws" ? 1 : 0
+
+  statement {
+    actions = [
+      "route53:ChangeResourceRecordSets",
+    ]
+
+    resources = [
+      var.aws_route53_zone_arn,
+    ]
+  }
+}
+
 # Policy allowing fetching code package from S3
-# TODO: Remove tflint-ignore in next PR
-# tflint-ignore: terraform_unused_declarations
 data "aws_iam_policy_document" "s3_bootstrap" {
   count = local.create_s3_bootstrap_policy ? 1 : 0
 
@@ -29,7 +49,7 @@ data "aws_iam_policy_document" "s3_bootstrap" {
     ]
 
     resources = [
-      "${data.aws_s3_bucket.bootstrap[0].arn}/*",
+      "${element(data.aws_s3_bucket.bootstrap, 1).arn}/*",
     ]
   }
 }
@@ -37,4 +57,120 @@ data "aws_iam_policy_document" "s3_bootstrap" {
 data "aws_s3_bucket" "bootstrap" {
   count  = local.create_s3_bootstrap_policy ? 1 : 0
   bucket = var.bootstrap_objectstorage_bucket_name
+}
+
+# Lookup AMI
+data "aws_ami" "ami" {
+  for_each = { for k, v in { "ubuntu" = "os"} : k => v if local.use_aws_ubuntu_ami }
+
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = local.ami_map[each.key].filters
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  # Canonical
+  owners = local.ami_map[each.key].owners
+}
+
+# Cloud-init config template
+data "template_cloudinit_config" "node" {
+  count = var.cloud_provider == "aws" ? 1 : 0
+
+  gzip = false
+
+  # TODO:  This will be getting modified to supply input to an external script
+  #  I have to build the cardano-node-ansible repo out before modifying this to be accurate
+  part {
+    filename     = "init.cfg"
+    content_type = "text/cloud-config"
+    content      = <<-EOF
+    #cloud-config
+    packages: ${join("\n  -", var.cloudinit_packages)}
+    runcmd: ${join("\n  -", var.cloudinit_runcmd)}
+    write_files:
+      - path: '/usr/local/bin/bootstrap.sh'
+        permissions: '0755'
+        owner: 'root:root'
+        content: "IGNORE_ME"
+    EOF
+  }
+}
+
+# Policy allowing attaching the node's EBS volume
+data "aws_iam_policy_document" "ebs_attach" {
+  count = local.use_aws_ebs_volumes ? 1 : 0
+
+  statement {
+    actions = [
+      "ec2:AttachVolume",
+    ]
+
+    resources = [
+      "*",
+    ]
+
+    condition {
+      test     = "StringLike"
+      variable = "ec2:ResourceTag/Name"
+
+      values = [
+        "${var.name}-node",
+      ]
+    }
+  }
+
+  statement {
+    actions = [
+      "ec2:DescribeVolumes",
+      "ec2:DescribeVolumeStatus",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+# Policy allowing attaching the node's EIP
+data "aws_iam_policy_document" "eip_attach" {
+  count = local.use_aws_external_ip ? 1 : 0
+
+  statement {
+    actions = [
+      "ec2:AssociateAddress",
+    ]
+
+    resources = [
+      "*",
+    ]
+
+    condition {
+      test     = "StringLike"
+      variable = "ec2:ResourceTag/Name"
+
+      values = [
+        "${var.name}-node",
+      ]
+    }
+  }
+
+  # TODO: narrow the scope
+  statement {
+    actions = [
+      "ec2:DescribeAddresses",
+      "ec2:DescribeInstances",
+      "ec2:DescribeNetworkInterfaces",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
 }
---
+++ ec2-asg.aws.tf
@@ -0,0 +1,107 @@
+locals {
+  keypair_name = local.create_aws_key_pair ? aws_key_pair.ssh[0].key_name : var.ssh_key_pair
+}
+
+# Launch templates
+resource "aws_launch_template" "node" {
+  count = var.cloud_provider == "aws" ? 1 : 0
+
+  name_prefix   = "${var.name}-node"
+  image_id      = data.aws_ami.ami[var.operating_system].image_id
+  instance_type = var.instance_type
+  key_name      = local.keypair_name
+
+  update_default_version = var.aws_launch_template_update_default_version
+
+  user_data = element(data.template_cloudinit_config.node, 1).rendered
+
+  disable_api_termination = var.aws_instance_termination_protection
+
+  vpc_security_group_ids = length(var.aws_security_group_ids) > 0 ? var.aws_security_group_ids : null
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.node[0].name
+  }
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size           = var.root_block_volume_size_gb
+      volume_type           = var.root_block_volume_type
+      delete_on_termination = var.root_block_volume_delete_on_termination
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name}-node"
+    },
+  )
+}
+
+# Data EBS volumes
+resource "aws_ebs_volume" "node_data" {
+  for_each = {
+    for key, val in toset(var.ebs_block_device) :
+    key => val if local.use_aws_ebs_volumes
+  }
+
+  availability_zone = each.value.availability_zone
+  encrypted             = lookup(each.value, "encrypted", null)
+  iops                  = lookup(each.value, "iops", null)
+  kms_key_id            = lookup(each.value, "kms_key_id", null)
+  size = lookup(each.value, "volume_size", null)
+  throughput            = lookup(each.value, "throughput", null)
+  type = lookup(each.value, "volume_type", null)
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name}-node"
+    },
+    lookup(each.value, "tags", {})
+  )
+}
+
+# Autoscaling groups
+# tflint-ignore: aws_resource_missing_tags
+resource "aws_autoscaling_group" "node" {
+  count = var.cloud_provider == "aws" ? 1 : 0
+
+  name = "${var.name}-node"
+
+  desired_capacity = 1
+  max_size         = 1
+  min_size         = 1
+
+  vpc_zone_identifier = var.instance_subnet_ids
+
+  enabled_metrics = [
+    "GroupInServiceInstances",
+    "GroupTerminatingInstances",
+  ]
+
+  launch_template {
+    id      = aws_launch_template.node[0].id
+    version = "$Latest"
+  }
+
+  health_check_type = "EC2"
+
+  dynamic "tag" {
+    for_each = merge(
+      var.tags,
+      {
+        Name              = "${var.name}-node"
+      },
+    )
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+}
